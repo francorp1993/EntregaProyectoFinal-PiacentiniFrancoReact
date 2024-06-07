bd552cf6396d6c377b98e07f47df619cfa45316b
francorp1993 <franco_piacentini10@hotmail.com>
2024-06-07 00:25:44 -0300

    agregado de carrousel para imgs + eliminicacion img innecesarias

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
-
+import React from 'react';
 
 export const Item = ({ amp }) => {
-
-    // const { agregarAlCarrito } = useContext(CartContext);
-
     return (
         <div className="item">
-            <img className='img' src={amp.foto} alt={amp.modelo} />
+            <div id={`carousel-${amp.id}`} className="carousel slide">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target={`#carousel-${amp.id}`} data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target={`#carousel-${amp.id}`} data-bs-slide-to="1" aria-label="Slide 2"></button>
+                </div>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src={amp.foto} className="d-block w-100" alt={amp.modelo}/>
+                    </div>
+                    <div className="carousel-item">
+                        <img src={amp.foto2} className="d-block w-100" alt={amp.modelo}/>
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target={`#carousel-${amp.id}`} data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target={`#carousel-${amp.id}`} data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
             <div className="info-amp">
                 <h2>{amp.modelo}</h2>
                 <p className='precio'>${amp.precio}</p>
                 <p className="data">{amp.efectos}</p>
             </div>
-
-            {/* <Link to={`/item/${amp.id}`}>Ver más</Link> */}
-            {/* <button onClick={() => agregarAlCarrito(amp)}>Agregar al carrito</button> */}
         </div>
-
-    )
-}
+    );
+};
